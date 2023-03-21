7a2571e178ea5ea577bab4bcef63e15a54f62690
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-03-21 14:12:55 -0400

    Game logic is complete

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 const player1Btn = document.getElementById('player1-rsp');
 const player2Btn = document.getElementById('player2-rsp');
 const reset = document.querySelector('.resetBtn');
-
 const rock = 'rock';
 const scissors = 'scissors';
 const paper = 'paper';
 const errorMsg = 'Something went wrong';
-
-const rockWin = './images/rock-win.jpeg';
+const rockPic = './images/rock-pic.jpeg';
 const scissorsPic = './images/scissors-pic.jpg';
-const paperWin = './images/paper-win.jpg';
-const tie = './images/tie-icon-15544.png'
-
-// let firstPlayerChoice;
-// let secondPlayerChoice;
-
-// const firstPossibility = firstPlayerChoice === rock && secondPlayerChoice === scissors;
-// const secondPossibility = firstPlayerChoice === scissors && secondPlayerChoice === paper;
-// const thirdPossibility = firstPlayerChoice === paper && secondPlayerChoice === rock;
-// const fourthPossibility = secondPlayerChoice === rock && firstPlayerChoice === scissors;
-// const fifthPossibility = secondPlayerChoice === scissors && firstPlayerChoice === paper;
-// const sixthPossibility = secondPlayerChoice === paper && firstPlayerChoice === rock;
-// const seventhPossibility = firstPlayerChoice === secondPlayerChoice;
+const paperPic = './images/paper-pic.jpg';
+let firstPlayerChoice;
+let secondPlayerChoice;
 
 
 player1Btn.addEventListener('click', () => { 
@@ -34,30 +22,29 @@ player2Btn.addEventListener('click', () => {
   player2Btn.style.backgroundColor = '#e0e0e0';
   player2Btn.style.color = '#636363';
   player2RandomNumber(); 
+  postResultsToDOM(firstPlayerChoice, secondPlayerChoice);
 }, {once: true});
 
 reset.addEventListener('click', () => window.location.reload());
 
-// Random Number rock-scissors-paper result
+
 function player1RandomNumber() {
+  let player1Choice = document.getElementById('player1-choice');
+
   const randomNum = Math.floor(Math.random() * 3 + 1);
-  let firstPlayerChoice;
 
   if (randomNum === 1) {
     firstPlayerChoice = rock;
-    const player1Choice = document.getElementById('player1-choice');
     const image = document.createElement('img');
-    image.setAttribute('src', rockWin);
+    image.setAttribute('src', rockPic);
     player1Choice.appendChild(image);
   } else if (randomNum === 2) {
     firstPlayerChoice = paper;
-    const player1Choice = document.getElementById('player1-choice');
     const image = document.createElement('img');
-    image.setAttribute('src', paperWin);
+    image.setAttribute('src', paperPic);
     player1Choice.appendChild(image);
   } else if (randomNum === 3) {
     firstPlayerChoice = scissors;
-    const player1Choice = document.getElementById('player1-choice');
     const image = document.createElement('img');
     image.setAttribute('src', scissorsPic);
     player1Choice.appendChild(image);
@@ -65,31 +52,29 @@ function player1RandomNumber() {
     console.log(errorMsg)
   }
 
-  console.log('Player one:', firstPlayerChoice)
+  console.log('Player one:', randomNum, firstPlayerChoice);
 
   return firstPlayerChoice;
 }
 
 
 function player2RandomNumber() {
+  let player2Choice = document.getElementById('player2-choice');
+
   const randomNum = Math.floor(Math.random() * 3 + 1);
-  let secondPlayerChoice;
 
   if (randomNum === 1) {
     secondPlayerChoice = rock;
-    const player2Choice = document.getElementById('player2-choice');
     const image = document.createElement('img');
-    image.setAttribute('src', rockWin);
+    image.setAttribute('src', rockPic);
     player2Choice.appendChild(image);
   } else if (randomNum === 2) {
     secondPlayerChoice = paper;
-    const player2Choice = document.getElementById('player2-choice');
     const image = document.createElement('img');
-    image.setAttribute('src', paperWin);
+    image.setAttribute('src', paperPic);
     player2Choice.appendChild(image);
   } else if (randomNum === 3) {
     secondPlayerChoice = scissors;
-    const player2Choice = document.getElementById('player2-choice');
     const image = document.createElement('img');
     image.setAttribute('src', scissorsPic);
     player2Choice.appendChild(image);
@@ -98,108 +83,71 @@ function player2RandomNumber() {
     return errorMsg
   }
 
-  console.log('Player two:', secondPlayerChoice)
+  console.log('Player two:', randomNum, secondPlayerChoice)
 
   return secondPlayerChoice;
 }
 
-
-// Link number to image
-function getResults(p1value, p2value) {
+function postResultsToDOM(p1value, p2value) {
 
   let resultMsg = document.querySelector('.result-Msg');
+  let changeMsgDivColor = document.querySelector('.result-Msg').style.backgroundColor = '#ffce35';
   let spanText = document.createElement('span');
   let player1Frame = document.querySelector('.player1-frame');
   let player2Frame = document.querySelector('.player2-frame');
 
-  const firstPossibility = p1value === rock && p2value === scissors;
-  const secondPossibility = p1value === scissors && p2value === paper;
-  const thirdPossibility = p1value === paper && p2value === rock;
-  const fourthPossibility = p2value === rock && p1value === scissors;
-  const fifthPossibility = p2value === scissors && p1value === paper;
-  const sixthPossibility = p2value === paper && p1value === rock;
-  const seventhPossibility = p1value === p2value;
-
-  let possiblities = firstPossibility || secondPossibility || thirdPossibility || fourthPossibility || fifthPossibility || sixthPossibility || seventhPossibility;
-
-  switch (possiblities) {
-    case firstPossibility:
-      resultMsg = document.querySelector('.result-Msg');
+  if (p1value === rock && p2value === scissors) {
       spanText = document.createElement('span');
       spanText.textContent = "Rock smashes scissors... Player 1 wins!";
       resultMsg.appendChild(spanText);
-      player1Frame = document.querySelector('.player1-frame');
       player1Frame.style.backgroundColor = '#3FBF7F';
-      player2Frame = document.querySelector('.player2-frame');
       player2Frame.style.backgroundColor = '#FF0000';
-      break;
-    case secondPossibility:
-      resultMsg = document.querySelector('.result-Msg');
+  } 
+  if (p1value === scissors && p2value === paper) {
       spanText = document.createElement('span');
       spanText.textContent = "Scissors cut paper... Player 1 wins!";
       resultMsg.appendChild(spanText);
-      player1Frame = document.querySelector('.player1-frame');
       player1Frame.style.backgroundColor = '#3FBF7F';
-      player2Frame = document.querySelector('.player2-frame');
       player2Frame.style.backgroundColor = '#FF0000';
-      break;
-    case thirdPossibility:
-      resultMsg = document.querySelector('.result-Msg');
-      spanText = document.createElement('span');
-      spanText.textContent = "Paper covers rock... Player 1 wins!";
-      resultMsg.appendChild(spanText);
-      player1Frame = document.querySelector('.player1-frame');
-      player1Frame.style.backgroundColor = '#3FBF7F';
-      player2Frame = document.querySelector('.player2-frame');
-      player2Frame.style.backgroundColor = '#FF0000';
-      break;
-    case fourthPossibility:
-      resultMsg = document.querySelector('.result-Msg');
-      spanText = document.createElement('span');
-      spanText.textContent = "Rock smashes scissors... Player 2 wins!";
-      resultMsg.appendChild(spanText);
-      player1Frame = document.querySelector('.player1-frame');
-      player1Frame.style.backgroundColor = '#FF0000';
-      player2Frame = document.querySelector('.player2-frame');
-      player2Frame.style.backgroundColor = '#3FBF7F';
-      break;
-    case fifthPossibility:
-      resultMsg = document.querySelector('.result-Msg');
-      spanText = document.createElement('span');
-      spanText.textContent = "Scissors cut paper... Player 2 wins!";
-      resultMsg.appendChild(spanText);
-      player1Frame = document.querySelector('.player1-frame');
-      player1Frame.style.backgroundColor = '#FF0000';
-      player2Frame = document.querySelector('.player2-frame');
-      player2Frame.style.backgroundColor = '#3FBF7F';
-      break;
-    case sixthPossibility:
-      resultMsg = document.querySelector('.result-Msg');
-      spanText = document.createElement('span');
-      spanText.textContent = "Paper covers rock... Player 2 wins!";
-      resultMsg.appendChild(spanText);
-      player1Frame = document.querySelector('.player1-frame');
-      player1Frame.style.backgroundColor = '#FF0000';
-      player2Frame = document.querySelector('.player2-frame');
-      player2Frame.style.backgroundColor = '#3FBF7F';
-      break;
-    case seventhPossibility:
-      resultMsg = document.querySelector('.result-Msg');
-      spanText = document.createElement('span');
-      spanText.textContent = `It's a tie - Player 1: ${firstPlayerChoice}, Player 1: ${secondPlayerChoice}`;
-      resultMsg.appendChild(spanText);
-      player1Frame = document.querySelector('.player1-frame');
-      player1Frame.style.backgroundColor = '#3FBF7F';
-      player2Frame = document.querySelector('.player2-frame');
-      player2Frame.style.backgroundColor = '#FF0000';
-      break;
-    default:
-      resultMsg = document.querySelector('.result-Msg');
-      spanText = document.createElement('span');
-      spanText.textContent = `${errorMsg}`;
-      resultMsg.appendChild(spanText);
-
-    }
-      
-}
-
+  } 
+  if (p1value === paper && p2value === rock) {
+    spanText = document.createElement('span');
+    spanText.textContent = "Paper covers rock... Player 1 wins!";
+    resultMsg.appendChild(spanText);
+    player1Frame.style.backgroundColor = '#3FBF7F';
+    player2Frame.style.backgroundColor = '#FF0000';
+  } 
+  if (p2value === rock && p1value === scissors) {
+    spanText = document.createElement('span');
+    spanText.textContent = "Rock smashes scissors... Player 2 wins!";
+    resultMsg.appendChild(spanText);
+    player1Frame.style.backgroundColor = '#FF0000';
+    player2Frame.style.backgroundColor = '#3FBF7F';
+  }
+  if (p2value === scissors && p1value === paper) {
+    spanText = document.createElement('span');
+    spanText.textContent = "Scissors cut paper... Player 2 wins!";
+    resultMsg.appendChild(spanText);
+    player1Frame.style.backgroundColor = '#FF0000';
+    player2Frame.style.backgroundColor = '#3FBF7F';
+  }
+  if (p2value === paper && p1value === rock) {
+    spanText = document.createElement('span');
+    spanText.textContent = "Paper covers rock... Player 2 wins!";
+    resultMsg.appendChild(spanText);
+    player1Frame.style.backgroundColor = '#FF0000';
+    player2Frame.style.backgroundColor = '#3FBF7F';
+  }
+  if (p1value === p2value) {
+    spanText = document.createElement('span');
+    spanText.textContent = "It's a tie";
+    resultMsg.appendChild(spanText);
+    player1Frame.style.backgroundColor = '#F0F0F0';
+    player2Frame.style.backgroundColor = '#F0F0F0';
+  }
+  else if (!p1value || !p2value) {
+    spanText = document.createElement('span');
+    spanText.textContent = `${errorMsg}`;
+    resultMsg.appendChild(spanText);
+  } 
+}      
